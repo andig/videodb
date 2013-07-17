3151cea20ed011cfa43f005a56d8b4ec1ab4e291
Andreas Goetz <cpuidle@gmx.de>
2013-07-17 18:06:03 +0200

    Fix #21 without breaking #11

--- lib/smarty/custom/function.html_image.php
+++ lib/smarty/custom/function.html_image.php
@@ -20,18 +20,17 @@ define(THUMB_CACHE_TARGET, true);
  */ 
 function truepath($path){ 
 	// whether $path is unix or not 
-	$unipath=strlen($path)==0 || $path{0}!='/';
-	// attempts to detect if path is relative in which case, add cwd 
-	if(strpos($path,':')===false && $unipath) {
-		$path=getcwd().DIRECTORY_SEPARATOR.$path;
-		$unipath=false;
+	$relpath=strlen($path)==0 || $path{0}!='/';
+	// attempts to detect if path is relative in which case, add cwd
+	if (strpos($path,':')===false && $relpath) {
+		$path = getcwd().DIRECTORY_SEPARATOR.$path;
 	}
 	// resolve path parts (single dot, double dot and double delimiters) 
 	$path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
 	$parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
 	$absolutes = array();
 	foreach ($parts as $part) { 
-	if ('.' == $part) continue;
+		if ('.' == $part) continue;
 		if ('..' == $part) { 
 			array_pop($absolutes);
 		} else { 
@@ -40,9 +39,12 @@ function truepath($path){
 	} 
 	$path=implode(DIRECTORY_SEPARATOR, $absolutes);
 	// resolve any symlinks 
-	if(file_exists($path) && linkinfo($path)>0)$path=readlink($path);
-	// put initial separator that could have been lost 
-	$path=!$unipath ? '/'.$path : $path;
+	if (file_exists($path) && linkinfo($path)>0) {
+		$path = readlink($path);
+	} else {
+		// put back initial unix separator that could have been lost 
+		$path = (strpos($path,':')>=0 || $relpath) ? $path : '/'.$path;
+	}
 	return $path;
 }
 
