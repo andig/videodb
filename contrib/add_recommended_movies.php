240b891eb892b24618b56e6fd71eb38d055a3aad
kec2 <klaus_edwin@hotmail.com>
2014-02-13 20:43:03 +0100

    resolved merge.

--- contrib/add_recommended_movies.php
+++ contrib/add_recommended_movies.php
@@ -25,13 +25,7 @@ session_write_close();
     <title>Find Movie Recommendations</title>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta name="description" content="VideoDB" />
-<<<<<<< HEAD
-<!--
-    <link rel="stylesheet" href="../templates/modern/compact.css" type="text/css" />
--->
-=======
 
->>>>>>> a2d143b80fbd2ebfa10efe5ca5c63b7cf7a89487
     <style>
         .green { color:green }
     </style>
@@ -66,16 +60,6 @@ if ($submit)
             continue;
         }
 
-<<<<<<< HEAD
-            $url = engineGetRecommendationsUrl($video['imdbID'], 'imdb');
-
-            $resp = httpClient($url, true);
-            if (!$resp['success'])
-            {
-                echo($resp['error']."<br/>");
-                continue;
-            }
-=======
         if (empty($data))
         {
             // sometimes there are no recommendations for a movie. This is true for Underworld: imdbId 0320691
@@ -91,55 +75,10 @@ if ($submit)
         foreach ($data as $recommended)
         {
             $available = (count(runSQL("SELECT * FROM ".TBL_DATA." WHERE imdbID like '%".$recommended['id']."'")) > 0);
->>>>>>> a2d143b80fbd2ebfa10efe5ca5c63b7cf7a89487
-
             if (!$available)
             {
-<<<<<<< HEAD
-                $title  = $recommend[2];
-                $id     = $recommend[1];
-
-                if (preg_match('/<img/', $title)) continue;
-
-                $rating = '';
-                if (preg_match("#$id.+?<b>(\d+\.\d+)</b>#i", $resp['data'], $match))
-                {
-                    $rating = $match[1];
-                    // matching at least required rating?
-                    if (!empty($required_rating) && ((float) $rating < $required_rating)) continue;
-                }
-
-                $year = '';
-                if (preg_match("#\((\d{4})\)#i", $title, $match))
-                {
-                    $year = $match[1];
-                    // matching at least required year?
-                    if (!empty($required_year) && ((int) $year < $required_year)) continue;
-                }
-
-                if (empty($rating) || ($rating >= $required_rating))
-                {
-                    $available = (count(runSQL("SELECT * FROM ".TBL_DATA." WHERE imdbID like '%$id'")) > 0);
-
-                    if ($available)
-                    {
-                        $add_movie = $title;
-                    }
-                    else
-                    {
-                        $add_movie = '<a class="green" href="../edit.php?save=1&mediatype='.MEDIA_WISHLIST.'&lookup=1&imdbID='.$id.
-                                     '&title='.urlencode($title).'" target="_blank">'.$title.' <img src="../images/add.gif" border="0"/></a>';
-                    }
-
-                    $add_movie = 'Recommended: '.$add_movie." (IMDB Id $id) $rating<br/>\n";
-                    echo $add_movie;
-
-                    if ($download && !$available) engineGetData($id);
-                }
-=======
                 $recommended['title'] = '<a class="green" href="../edit.php?save=1&mediatype='.MEDIA_WISHLIST.'&lookup=1&imdbID='.$recommended['id'].
                              '&title='.urlencode($recommended['title']).'" target="_blank">'.$recommended['title'].' <img src="../images/add.gif" border="0"/></a>';
->>>>>>> a2d143b80fbd2ebfa10efe5ca5c63b7cf7a89487
             }
 
             echo "<tr>";
