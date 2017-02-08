af5a56db682cf6ade0fc29d74b2210d32c450502
kec2 <klaus_edwin@hotmail.com>
2017-02-08 22:17:12 +0100

    Fixed issue with missing country

--- engines/imdb.php
+++ engines/imdb.php
@@ -272,7 +272,7 @@ function imdbData($imdbID)
 
     // Countries
     preg_match('/Country:\s*<\/h4>(.+?)<\/div>/si', $resp['data'], $ary);
-    preg_match_all('/<a href=".*?countries.*?".*?>(.+?)<\/a>/si', $ary[1], $ary, PREG_PATTERN_ORDER);
+    preg_match_all('/<a.+?country.+?>(.+?)<\/a>/si', $ary[1], $ary, PREG_PATTERN_ORDER);
     $data['country'] = trim(join(', ', $ary[1]));
 
     // Languages
@@ -305,7 +305,7 @@ function imdbData($imdbID)
         # country
         if (!$data['country']) {
             preg_match('/Country:\s*<\/h4>(.+?)<\/div>/si', $sresp['data'], $ary);
-            preg_match_all('/<a href=".*?countries.*?".*?>(.+?)<\/a>/si', $ary[1], $ary, PREG_PATTERN_ORDER);
+            preg_match_all('/<a.+?country.+?>(.+?)<\/a>/si', $ary[1], $ary, PREG_PATTERN_ORDER);
             $data['country'] = trim(join(', ', $ary[1]));
         }
 
